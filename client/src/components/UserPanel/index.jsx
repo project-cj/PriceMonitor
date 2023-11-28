8a8a7586f7116eef3e19531c3dd29dc02704d172
Kuba <dabrowskikuba.kd@gmail.com>
2023-11-28 19:44:21 +0100

    fix user panel diffs from admin user panel

--- client/src/components/UserPanel/index.jsx
+++ client/src/components/UserPanel/index.jsx
@@ -23,12 +23,14 @@ const UserPanel = () => {
   let decode = null;
   decode = jwt(user);
   const id = decode.id;
+  const status = decode.status;
 
   const [error, setError] = useState(null);
   const [searchResults, setSearchResults] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const [radius, setRadius] = useState(0);
   const [position, setPosition] = useState(null)
+  const [shopProposals, setShopProposals] = useState([]);
 
   function LocationMarker() {
     const map = useMapEvents({
@@ -52,8 +54,17 @@ const UserPanel = () => {
       setError("Wystąpił błąd podczas pobierania danych.");
     }
   };
+  const fetchShopProposals = async () => {
+    try {
+        const response = await axios.get("http://localhost:8080/api/admin/getproposals");
+        setShopProposals(response.data);
+    } catch (error) {
+        setError("Wystąpił błąd podczas pobierania propozycji sklepów.");
+    }
+}
   useEffect(() => {
     fetchUserData();
+    fetchShopProposals();
     console.log('res',searchResults)
   }, []);
 
@@ -142,6 +153,9 @@ const UserPanel = () => {
     console.log("Pr ID:", item, "Sh ID:",item2)
     navigate('/product', {state: {item, item2}})
   }
+  const navigateEdit = (proposal) => {
+    navigate('/admin/shopproposal/edit', {state: {proposal}})
+  }
   
 
   if(isLoading){
@@ -158,75 +172,110 @@ const UserPanel = () => {
           {error && <p className={styles.error}>{error}</p>}
           {searchResults && 
             <div className={styles.shop_container_2}>
-              <p className={styles.userDetails}>E-mail: {searchResults.email}</p>
-              <p className={styles.userDetails}>Pseudonim: {searchResults.alias}</p>
-              <p className={styles.userDetails}>Promień wyszukiwania sklepów: {searchResults.radius} km</p>
-              <p className={styles.userDetails}>Podaj nowy promień</p>
-              <input className={styles.number_field} type="number" value={radius} onChange={(e) => setRadius(e.target.value)}/>
-              <button onClick ={handleChangeRadius} className={styles.green_btn} >Zmień promień</button><br/>
-              <button onClick={() =>navigateAlias(id)} className={styles.green_btn}>Zmień pseudonim</button>
-              <button onClick={() =>navigatePassword(id)} className={styles.green_btn}>Zmień hasło</button><br/>
-              <p className={styles.title}>Twoja lokalizacja</p>
-              <p>Możesz zmienić lokalizację naciskając nowe miejsce na mapie i potwierdzic przyciskiem 'Zmień lokalizację'</p>
-              <MapContainer style={{height: '300px', width: '50%'}} center={position} zoom={16} className={styles.mapContainer}>
-                <TileLayer
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position} icon={icon}>
-                  <Popup>
-                    {searchResults.alias}
-                  </Popup>
-                </Marker>
-                <LocationMarker />
-              </MapContainer>
-              <button onClick ={handleChangeLocation} className={styles.green_btn} >Zmień lokalizację</button><br/>
-              <p className={styles.title}>Twoje listy zakupów</p>
-              <table className={styles.searchResults}>
-                <thead>
-                  <tr>
-                    <th>Nazwa</th>
-                    <th>Przybliżona cena</th>
-                    <th>Usuń listę</th>
-                    <th>Przejdź do listy</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {searchResults.shoppinglists.map((result, index) => (
-                    <tr key={index}>
-                      <td>{result.name}</td>
-                      <td>{result.price}</td>
-                      <td className={styles.navigateButton}><img src={bin} onClick={() =>handleDeleteList(result.id)} alt="x"></img></td>
-                      <td className={styles.navigateButton}><img src={vectorRight} onClick={() =>navigateShoppingList(result.id)} alt="x"></img></td> 
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-              <p className={styles.title}>Twoje dodane ceny produktów</p>
-              <table className={styles.searchResults}>
-                <thead>
-                  <tr>
-                    <th>Produkt</th>
-                    <th>Cena</th>
-                    <th>Potwierdzenia</th>
-                    <th>Odrzucenia</th>
-                    <th>Usuń cenę</th>
-                    <th>Przejdź do produktu</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {searchResults.price_reads.map((result, index) => (
-                    <tr key={index}>
-                      <td>{result.Shop_has_Product.Product.name}</td>
-                      <td>{result.price}</td>
-                      <td>{result.confirmation_number}</td>
-                      <td>{result.rejected_number}</td>
-                      <td className={styles.navigateButton}><img src={bin} onClick={() =>handleDelete(result.id)} alt="x"></img></td>
-                      <td className={styles.navigateButton}><img src={vectorRight} onClick={() =>navigateProduct(result.Shop_has_Product.Product_id, result.Shop_has_Product.Shop_id)} alt="x"></img></td>  
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+              {status === "ADMIN" ? (
+                <div>
+                  <p className={styles.userDetails}>E-mail: {searchResults.email}</p>
+                  <p className={styles.userDetails}>Pseudonim: {searchResults.alias}</p>
+                  <p className={styles.title}>Propozycje sklepów</p>
+                  {shopProposals.length>0 && 
+                    <div>
+                      <table className={styles.searchResults}>
+                        <thead>
+                          <tr>
+                            <th>Nazwa sklepu</th>
+                            <th>X</th>
+                            <th>Y</th>
+                            <th>Edycja</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {shopProposals.map((proposal, index) => (
+                            <tr key={index}>
+                              <td>{proposal.name}</td>
+                              <td>{proposal.x_location}</td>
+                              <td>{proposal.y_location}</td>
+                              <td className={styles.navigateButton}><img src={vectorRight} onClick={() => navigateEdit(proposal)} alt="x"></img></td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  }
+                </div>
+              ) : (
+                <div>
+                  <p className={styles.userDetails}>E-mail: {searchResults.email}</p>
+                  <p className={styles.userDetails}>Pseudonim: {searchResults.alias}</p>
+                  <p className={styles.userDetails}>Promień wyszukiwania sklepów: {searchResults.radius} km</p>
+                  <p className={styles.userDetails}>Podaj nowy promień</p>
+                  <input className={styles.number_field} type="number" value={radius} onChange={(e) => setRadius(e.target.value)}/>
+                  <button onClick ={handleChangeRadius} className={styles.green_btn} >Zmień promień</button><br/>
+                  <button onClick={() =>navigateAlias(id)} className={styles.green_btn}>Zmień pseudonim</button>
+                  <button onClick={() =>navigatePassword(id)} className={styles.green_btn}>Zmień hasło</button><br/>
+                  <p className={styles.title}>Twoja lokalizacja</p>
+                  <p>Możesz zmienić lokalizację naciskając nowe miejsce na mapie i potwierdzic przyciskiem 'Zmień lokalizację'</p>
+                  <MapContainer style={{height: '300px', width: '50%'}} center={position} zoom={16} className={styles.mapContainer}>
+                    <TileLayer
+                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={position} icon={icon}>
+                      <Popup>
+                        {searchResults.alias}
+                      </Popup>
+                    </Marker>
+                    <LocationMarker />
+                  </MapContainer>
+                  <button onClick ={handleChangeLocation} className={styles.green_btn} >Zmień lokalizację</button><br/>
+                  <p className={styles.title}>Twoje listy zakupów</p>
+                  <table className={styles.searchResults}>
+                    <thead>
+                      <tr>
+                        <th>Nazwa</th>
+                        <th>Przybliżona cena</th>
+                        <th>Usuń listę</th>
+                        <th>Przejdź do listy</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {searchResults.shoppinglists.map((result, index) => (
+                        <tr key={index}>
+                          <td>{result.name}</td>
+                          <td>{result.price}</td>
+                          <td className={styles.navigateButton}><img src={bin} onClick={() =>handleDeleteList(result.id)} alt="x"></img></td>
+                          <td className={styles.navigateButton}><img src={vectorRight} onClick={() =>navigateShoppingList(result.id)} alt="x"></img></td> 
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  <p className={styles.title}>Twoje dodane ceny produktów</p>
+                  <table className={styles.searchResults}>
+                    <thead>
+                      <tr>
+                        <th>Produkt</th>
+                        <th>Cena</th>
+                        <th>Potwierdzenia</th>
+                        <th>Odrzucenia</th>
+                        <th>Usuń cenę</th>
+                        <th>Przejdź do produktu</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {searchResults.price_reads.map((result, index) => (
+                        <tr key={index}>
+                          <td>{result.Shop_has_Product.Product.name}</td>
+                          <td>{result.price}</td>
+                          <td>{result.confirmation_number}</td>
+                          <td>{result.rejected_number}</td>
+                          <td className={styles.navigateButton}><img src={bin} onClick={() =>handleDelete(result.id)} alt="x"></img></td>
+                          <td className={styles.navigateButton}><img src={vectorRight} onClick={() =>navigateProduct(result.Shop_has_Product.Product_id, result.Shop_has_Product.Shop_id)} alt="x"></img></td>  
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              )}
+              
             </div>
           }
         </div>
